90e4cc1583497d7d7b6345d2bce1e624a7f58a74
Eliana Romero <romeroelianaines@gmail.com>
2023-10-05 19:42:42 -0300

    Clase 1

---
+++ VARIABLES/Ejercicios clase 1/index.js
@@ -0,0 +1,75 @@
+// Ejercicio 1:
+
+// Declarar variables con datos de un usuario (user, age, email)
+// Mostrar estos datos por consola
+// Mostrar el tipo de dato de cada variable por consola
+
+
+// let firstname = "Nicole"
+// let lastname = "Suarez"
+// let age = 25
+// let email = "nicolesuarez@gmail.com"
+
+// console.log(firstname)
+// console.log(lastname)
+// console.log(age)
+// console.log(email)
+
+// console.log(typeof firstname)
+// console.log(typeof lastname)
+// console.log(typeof age)
+// console.log(typeof email)
+
+
+// ---------------------------------------------------------------------------------------------------------
+
+// Ejercicio 2:
+// Pedir al usuario sus datos personales (nombre, edad, pais, profesion)
+// Mostrar esos datos por consola usando concatenacion
+// Mostrar por consola la edad + 10
+
+// const name = prompt("Ingresa tu nombre de usuario:")
+// const age = Number(prompt("Ingresa tu edad:"))
+// const country = prompt("Ingresa tu pais:")
+// const  profession = prompt("Ingresa tu profesion:")
+// const addage = 10
+
+
+// console.log(`${name} ${age} ${country} ${profession}`)
+
+// console.log("La suma es:", age + addage)
+
+// // ---------------------------------------------------------------------------------------------------------------
+
+// Ejercicio 3:
+// Tomar las variables del ejercicio 1 y mostrar un texto por consola, por el documento y por un alert (hagan de a uno por vez, comentando y probando) y utilizar template literals. Recordar: document.write() y alert()
+
+// let firstname = "Nicole"
+// let lastname = "Suarez"
+// let age = 25
+// let email = "nicolesuarez@gmail.com"
+
+// console.log("Mi nombre es", `${firstname} ${lastname}`,"tengo", `${age}`,"años y mi mail de contacto es", `${email}`)
+// document.write("Hola mi nombre es Nicole Surez, tengo 25 años y mi maol de contacto es nicolesuarez@gmail.com ")
+// alert("Hola mi nombre es Nicole Surez, tengo 25 años y mi maol de contacto es nicolesuarez@gmail.com ")
+// console.log("Bienvenida ejecutada")
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// Ejercicio 4:
+// Pedir al usuario dos numeros y mostrar por el documento los resultados de cada calculo aritmetico. Ejemplo: El resultado de la suma es: ${resultado}
+
+// const one = Number(prompt("Ingrese el primer numero:"))
+// const two = Number(prompt("Ingrese el segundo numero:"))
+
+// console.log("La suma es:", one + two)
+// console.log("La resta es:", one - two)
+// console.log("La producto es:", one * two)
+// const result = one / two
+// console.log("La division es:", parseInt(result))
+// console.log("El resto es:", one % two)
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// Bonus:
+// A lo que hiciste en el ejercicio 4, sumale un tercer numero a cada calculo. Mostrar por consola y analizar que paso en cada caso. Obtuviste los resultados esperados?
